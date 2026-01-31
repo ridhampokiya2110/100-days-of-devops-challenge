aa083706f1ce008116e18f24f30e315202d3d3a5
ridhampokiya2110 <ridhampokiya10@gmail.com>
2026-02-01 00:53:03 +0530

    .

---
+++ Day-1/hello_devops.sh
@@ -0,0 +1,11 @@
+# Day 1: My First DevOps Script
+echo "-----------------------------------"
+echo "Welcome to the 100 Days of DevOps Challenge!"
+echo "Today is Day 1."
+echo "-----------------------------------"
+
+echo "Checking System Uptime..."
+uptime
+
+echo "-----------------------------------"
+echo "Script execution complete!"
