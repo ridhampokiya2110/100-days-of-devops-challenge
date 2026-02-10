37a2b569b6b09f423f1b0a6b1f65e056f7b1bf1d
ridhampokiya2110 <ridhampokiya10@gmail.com>
2026-02-10 22:43:50 +0530

    Day 10: Built Log Analyzer using awk

---
+++ Day-10/log_analyzer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+log_file="server.log"
+
+echo "analyzing log file: $log_file"
+echo "============================="
+
+#check if file exists
+if [ ! -f "$log_file" ]; then 
+    echo "Error: log file not found...!!!"
+    exit 1
+fi
+
+# 1. Print only the Date ($1) and the Message ($4 onwards)
+#awk automatically splits lines by spaces
+echo "---Full Summary---"
+awk '{print $1, $3, $4, $5, $6}' "$log_file"
+echo "====================================="
+
+#filter only errors
+echo "---Critical Errors Only---"
+awk '$3 == "ERROR" {print $0}' "$log_file"
+
+echo "==================================="
+
+error_count=$(awk '$3 == "ERROR" {count++} END {print count}' "$log_file")
+echo "total error found: $error_count"
