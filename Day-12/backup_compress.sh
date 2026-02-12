e56fc054f5d93788cf1c04b0d641eac974e00dfa
ridhampokiya2110 <ridhampokiya10@gmail.com>
2026-02-12 23:49:16 +0530

    Day 12: Created Compression Script with tar

---
+++ Day-12/backup_compress.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+source_dir="source_code"
+backup_file="backup_data.tar.gz"
+
+echo "Starting Backup Process"
+echo "======================="
+
+#check if source folder exists 
+if [ ! -d "$source_dir" ]; then
+    echo "Error folder not found..!!!"
+    exit 1
+fi
+
+echo "compressing file..."
+tar -czf "$backup_file" "$source_dir"
+
+echo "================================"
+echo "Backup Successful..............."
+
+#compare size
+original=$(du -sh "$source_dir" | cut -f1)
+compressed=$(du -sh "$backup_file" | cut -f1)
+
+echo "Original Size: $original"
+echo "Compressed Size: $compressed"
+
+echo "======================================"
